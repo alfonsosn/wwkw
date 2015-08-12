eacd3ea47c45cdb5554ce57bffba21204a28948f
Alfonso Enriquez Castro <alfonso.en.ca@gmail.com>
2015-08-11 20:59:13 -0400

    transition

--- browser/js/common/factories/RandomGreetings.js
+++ browser/js/common/factories/RandomGreetings.js
@@ -7,9 +7,11 @@ app.factory('RandomGreetings', function () {
     var greetings = [
         '"We all selfconscious I’m just the first to admit it."',
         '"We buy a lot of clothes when we don\'t really need them."',
-        '"I am God\'s vessel."',
+        '"Would you believe in what you what believe if you were the only one?"',
         '"I think  Kanye West is something similar to what Steve Jobs means."',
-        '"I hate when I\'m on a flight and I wake up with a water bottle next to me."'
+        '"I hate when I\'m on a flight and I wake up with a water bottle next to me."',
+        '"Oh great now I gotta be responsible for this water bottle."',
+        '"Sometimes I get emotional about fonts."'
     ];
 
     return {
